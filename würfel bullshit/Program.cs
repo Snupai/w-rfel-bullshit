c54110df4d048f3a8569923d985cf114b7ec77f7
Yann <yannluca@naeher.eu>
2023-07-14 08:57:52 +0200

    Projektdateien hinzufügen.

---
+++ würfel bullshit/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+using static Yann.wc;
+
+namespace würfel_bullshit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Würfel w = new Würfel(5);
+            Würfel w2 = new Würfel(9);
+            for (int i = 0; i < 10; i++)
+            {
+                w.Würfeln();
+                w2.Würfeln();
+                if (w.Augenzahl == w2.Augenzahl)
+                {
+                    WriteLineColor($"Würfel 1: <*green*>{w.Augenzahl}<*/*>  Würfel 2: <*green*>{w2.Augenzahl}<*/*>");
+                }
+                else
+                {
+                    WriteLineColor($"Würfel 1: <*red*>{w.Augenzahl}<*/*>  Würfel 2: <*red*>{w2.Augenzahl}<*/*>");
+                }
+            }
+            ReadKey();
+        }
+
+        static void Test()
+        {
+            //
+            // This program demonstrates all colors and backgrounds.
+            //
+            Type type = typeof(ConsoleColor);
+            ForegroundColor = ConsoleColor.White;
+            foreach (var name in Enum.GetNames(type))
+            {
+                BackgroundColor = (ConsoleColor)Enum.Parse(type, name);
+                WriteLine(name);
+            }
+            BackgroundColor = ConsoleColor.Black;
+            foreach (var name in Enum.GetNames(type))
+            {
+                ForegroundColor = (ConsoleColor)Enum.Parse(type, name);
+                WriteLine(name);
+            }
+
+            //
+            // This is an example to show you how to you WriteColor or WriteLineColor
+            //
+            WriteLineColor("\nYou can see all possible colors above!\nThis is an example string for <*!red*>background color<*/!*> and <*red*>foreground color<*/*>.");
+        }
+    }
+}
---
+++ würfel bullshit/Würfel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace würfel_bullshit
+{
+    internal class Würfel
+    {
+        private int MAX;
+        static Random rnd = new Random();
+
+        public Würfel()
+        {
+            MAX = 6;
+        }
+
+        public Würfel(int max)
+        {
+            if (max<2)
+            {
+                throw new Exception("Würfel muss mindestens 2 Seiten haben!");
+            }
+            MAX = max;
+        }
+
+        public int Augenzahl { get; private set; }
+        public int Würfeln()
+        {
+            return Augenzahl = rnd.Next(1, MAX + 1);
+        }
+    }
+}
